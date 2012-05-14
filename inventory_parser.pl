b41df1d435b130f19e693bcdae6f61747afb2932
jctong <jctong@jctong-desktop.(none)>
2012-05-14 15:35:04 -0700

    working prototype! this is the output:
    $ ./inventory_parser.pl
     You have: some cloth armor, a club, some leather boots, a leather cap, a
      small wooden shield.  Inventory weight is 18 lbs.
     (23 H 2 M):$

--- inventory_parser.pl
+++ inventory_parser.pl
@@ -4,22 +4,51 @@ use warnings;
 use strict;
 use Net::Telnet;
 
-my $t = new Net::Telnet (Timeout => 10, Port => 4040,);
+#read list of storage chars and passwords
+#format:
+#comments are lines starting with # (they are ignored)
+#one char name/password per line like this:
+#char_name:password
 
-$t->open("isengard.nazgul.com");
 
+open(ROSTER,"/secret/inventory_parser_secret.pl") or die "cant open secret file";
+
+my %roster;
+my $mud_ip="isengard.nazgul.com";
+my $mud_port=4040;
+
+while(defined(my $line=<ROSTER>)){
+	if($line =~ /^#/){
+		next;
+	}else{
+		chomp($line);
+		my ($name,$password)=split(/:/,$line);
+		$roster{$name}="$password";
+	}
+}
+
+foreach my $name (keys %roster){
+
+	my $t = new Net::Telnet (Timeout => 10, Port => $mud_port,);
+
+	$t->open($mud_ip);
+
+#this is what the login looksl like
 #Please enter name:
 #Please enter password:
 #(23 H 2 M):
 
-$t->waitfor('/Please enter name:/');
-$t->print("druuge");
+	$t->waitfor('/Please enter name:/');
+	$t->print($name);
 
 
-$t->waitfor('/Please enter password:/');
-$t->print("");
+	$t->waitfor('/Please enter password:/');
+	$t->print($roster{$name});
 
-
-my @lines = $t->cmd("i");
-
-print @lines;
+	$t->waitfor('/\(23 H 2 M\):/');
+	$t->print("rem all");
+	$t->waitfor('/\(23 H 2 M\):/');
+	$t->print("i");
+	my @inventory = $t->waitfor('/\(23 H 2 M\):/');
+	print "@inventory";
+}
