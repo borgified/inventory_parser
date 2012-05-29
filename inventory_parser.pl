1f26249c63e23733adbb092ca43bb3b41a6308ab
borgified <borgified@gmail.com>
2012-05-28 17:22:57 -0700

    still need to unpluralize words so they can be counted correctly also need to see at what point literal numbers become numerical ones

--- inventory_parser.pl
+++ inventory_parser.pl
@@ -6,38 +6,71 @@ use Net::Telnet;
 
 &main;
 
-sub main{
+sub main {
 
 #	&login;		#tmp uncommented
-	&parse;		
+#	&parse;		
+
+
+### THE PLAN
+#1. get login credentials
+
+#2. get inventory
+
+#3. store results
+
+
+		my $mud_ip = "isengard.nazgul.com";
+		my $mud_port=4040;
+
+		my $login = &retrieve_login_info;
+
+		foreach my $name (keys %{$login}){
+				my $inventory = &login($mud_ip,$mud_port,$name,$$login{$name});
+				print "-----------------\n";
+				print "$inventory\n";
+				print "-----------------\n";
+				&parse_items($inventory);
+		}
 }
 
-sub login{
 
+
+#input: login info (text file or database)
+#output: \$roster{$name}=$password
+
+#can be from a file or from database
 #read list of storage chars and passwords
 #format:
 #comments are lines starting with # (they are ignored)
 #one char name/password per line like this:
 #char_name:password
+sub retrieve_login_info {
 
+		open(ROSTER,"/secret/inventory_parser_secret.pl") or die "cant open secret file";
 
-	open(ROSTER,"/secret/inventory_parser_secret.pl") or die "cant open secret file";
+		my %roster;
 
-	my %roster;
-	my $mud_ip="isengard.nazgul.com";
-	my $mud_port=4040;
-
-	while(defined(my $line=<ROSTER>)){
-		if($line =~ /^#/){
-			next;
-		}else{
-			chomp($line);
-			my ($name,$password)=split(/:/,$line);
-			$roster{$name}="$password";
+		while(defined(my $line=<ROSTER>)){
+				if($line =~ /^#/){
+						next;
+				}else{
+						chomp($line);
+						my ($name,$password)=split(/:/,$line);
+						$roster{$name}="$password";
+				}
 		}
-	}
 
-	foreach my $name (keys %roster){
+		return \%roster;
+
+}
+
+
+#input: $ip $port $name $password
+#output: \@inventory
+sub login{
+
+		my($mud_ip,$mud_port,$name,$password)=@_;
 
 		my $t = new Net::Telnet (Timeout => 10, Port => $mud_port,);
 
@@ -53,80 +86,92 @@ sub login{
 
 
 		$t->waitfor('/Please enter password:/');
-		$t->print($roster{$name});
+		$t->print($password);
 
 		$t->waitfor('/\(\d+ H \d+ M\):/');
 		$t->print("rem all");
 		$t->waitfor('/\(\d+ H \d+ M\):/');
+
+TRYAGAIN:
+
 		$t->print("i");
 		my @inventory = $t->waitfor('/\(\d+ H \d+ M\):/');
-		my $inv_ref=\@inventory;
-		&parse($inv_ref);
-	}
+		
+		my $items=&is_valid(\@inventory);
+		if($items ne 'error'){
+				return $items;
+		}else{
+				goto TRYAGAIN;
+		}
 }
 
-sub parse{
-#You have: an aquamarine potion, a blue bubbly potion, two dark flasks, some
-#  eye of newt, a golden dagger, three green potions, a hammer of
-#  thunderbolts, two hazy potions, an ivory coffer, a verdant green scroll,
-#  nine wand of the efreetis, three werewolf sire skulls.  Inventory weight
-#  is 248 lbs.
-#(120 H 3 M):
+#input can be corrupted if the inventory listing gets mixed up with
+#other text output. like when a mob walks into the room just at the right time
 
-#	my($inv_ref)=@_;		#temporary uncomment 
-
-#	my @inventory = @{$inv_ref};	#tmp uncomment
-
-#	open(OUTPUT,">output.txt"); 	#tmp uncomment 
-#	print OUTPUT "@inventory";	#tmp uncomment
-	open(INPUT,"output.txt");
-
+#input: inventory listing
+#output: $items string if ok, 0 if corrupted
+sub is_valid{
+		my $result="";
+		my ($inv_aref)=@_;
 
 #reconstruct inventory lines
-	my $result="";
-	my @inventory=<INPUT>;
-	foreach my $line (@inventory){
-		$line=~s/\(\d+ H \d+ M\)://g;
-		$line=~s/\n|\r//g;
-		$result=$result.$line;
-	}
+		foreach my $line (@$inv_aref){
+				$line=~s/\(\d+ H \d+ M\)://g;
+						$line=~s/\n|\r//g;
+						 $result=$result.$line;
+		}
 
-	$result =~ /You have: (.*) Inventory weight is (\d+) lbs./;
-	print "items: $1\nweight: $2\n";
-	print "##############################\n";
-	&parse_items($1);
-	
-#sample output
-#items: two Ahrot's magic strings (+1), two Medallion of Durins, an  aquamarine potion (M), a blue bubbly potion (M), a crown of foam, two dark  flasks (M), two sets of eye of newt (M), two galvorn rings, a galvorn  shield, a giant hammer of thunder (+3), a grey scroll (M), two hazy  potions (M), an imeril leggings (+1), some imeril sleeves (+1), an ivory  coffer, a knapsack, a mask of distortion, some mithril lamella armor, some  obsidian gauntlets, a silver hoop, two sundorian tassles, some volcanic  boots (+1), four wand of the efreetis (M). 
-#weight: 182
-
-
-
+		$result =~ /You have: (.*)\.  Inventory weight is (\d+) lbs./;
+		my $items = $1;
+		if($items !~ /\.|just arrived/){
+				return $items;
+		}
+		return "error";
 }
+
+
 
 sub parse_items{
-	my %keywords;
-	
-	$keywords{'some'}=1;
-	$keywords{'a'}=1;	
-	$keywords{'an'}=1;	
-	$keywords{'the'}=1;	
+		my %keywords;
 
-	my($parse_items)=@_;
+		$keywords{'some'}=1;
+		$keywords{'a'}=1;	
+		$keywords{'an'}=1;	
+		$keywords{'the'}=1;
+		$keywords{'two'}=2;
+		$keywords{'three'}=3;
+		$keywords{'four'}=4;
+		$keywords{'five'}=5;
+		$keywords{'six'}=6;
+		$keywords{'seven'}=7;
+		$keywords{'eight'}=8;
+		$keywords{'nine'}=9;
+		$keywords{'ten'}=10;
+		$keywords{'eleven'}=11;
+		$keywords{'twelve'}=12;
+		$keywords{'thirteen'}=13;
+		$keywords{'forteen'}=14;
+		$keywords{'fifteen'}=15;
+		$keywords{'sixteen'}=16;
+		$keywords{'seventeen'}=17;
+		$keywords{'eighteen'}=18;
+		$keywords{'nineteen'}=19;
 
-	$parse_items=~s/,\s+/,/g;
-	$parse_items=~s/\s+/ /g;
-	$parse_items=~s/\.//g;
-	$parse_items=~s/\s+$//g;
-	my @inventory=split(/,/,$parse_items);
+		my($parse_items)=@_;
 
-	foreach my $item(@inventory){
-		foreach my $k(keys %keywords){
-			if($item=~/^$k\s(.*)/){
-				print "$item ==> [$keywords{$k} $1]\n";
-			}
+		$parse_items=~s/,\s+/,/g;
+		$parse_items=~s/\s+/ /g;
+		$parse_items=~s/\s+$//g;
+		$parse_items=~s/sets of //g;
+
+		my @inventory=split(/,/,$parse_items);
+
+		foreach my $item(@inventory){
+				foreach my $k(keys %keywords){
+						if($item=~/^$k\s(.*)/){
+								print "$item ==> [$keywords{$k} $1]\n";
+						}
+				}
 		}
-	}
 }
-
 
